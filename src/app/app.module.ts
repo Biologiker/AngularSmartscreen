a776926f63caac11f60d4c2846634ef00ca471ce
DerBiologiker <quentink@gmx.de>
2022-11-30 17:36:20 +0100

    - Weather widget: Uhr Funktioniert, Tag wird geupdatet - Außerdem ist das Design für den Wetter Teil eingebaut

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { WeatherWidgetComponent } from './weather-widget/weather-widget.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    MainPageComponent,
+    WeatherWidgetComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [MainPageComponent, WeatherWidgetComponent, ToolbarComponent]
 })
 export class AppModule { }
---
+++ src/app/weather-widget/weather-widget.component.ts
@@ -0,0 +1,98 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskTimer } from 'tasktimer';
+
+@Component({
+  selector: 'app-weather-widget',
+  templateUrl: './weather-widget.component.html',
+  styleUrls: ['./weatherBox.weather-widget.component.scss', './timeBox.weather-widget.component.scss']
+})
+export class WeatherWidgetComponent implements OnInit {
+  ngOnInit() {
+    const shortWeekday = ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"];
+
+    //todo: muss noch geschleift werden
+    //Hier wird überprüft ob der aktuelle Tag mit dem dargestellten Tag übereinstimmt
+    if (document.getElementById("innerDayBox")!.innerHTML != shortWeekday[new Date().getDay()]) {
+      //Wenn er es nicht tut wird das innerHtml verändert zum shortCode für den WochenTag
+      document.getElementById("innerDayBox")!.innerHTML = shortWeekday[new Date().getDay()];
+    }
+
+    const timer = new TaskTimer(500);
+
+    const currDate: Date = new Date()
+
+    refreshSeconds('second', currDate.getSeconds().toString(), currDate.getMilliseconds() >= 500 /*Dieses If schaltet doMove an oder aus damit der Rythmus des Switchens gleich bleibt*/, true);
+
+    refreshSeconds('minute', currDate.getMinutes().toString(), currDate.getMilliseconds() >= 500, document.getElementById("secondLeft")!.classList.contains('move'));
+
+    refreshSeconds('hour', currDate.getHours().toString(), currDate.getMilliseconds() >= 500, document.getElementById("secondLeft")!.classList.contains('move'));
+
+    timer.on("tick", () => refreshWrapper(new Date()));
+
+    timer.start();
+  }
+}
+
+function refreshSeconds(element: string, timeElement: string, doMove: boolean, parentHasMove: boolean) { //NOSONAR
+  const elementLeft = document.getElementById(element + "Left");
+  const elementRight = document.getElementById(element + "Right");
+  const elementLeft2 = document.getElementById(element + "Left2");
+  const elementRight2 = document.getElementById(element + "Right2");
+
+  let elementLeftClassList = elementLeft!.classList
+  let elementRightClassList = elementRight!.classList
+  let elementLeft2ClassList = elementLeft2!.classList
+  let elementRight2ClassList = elementRight2!.classList
+
+  if (timeElement.length == 2) {
+    //Hier setzte ich die Zahlen ein für die oberen Zahlen ein
+    elementLeft!.innerHTML = timeElement[0];
+    elementRight!.innerHTML = timeElement[1];
+    //Hier die unteren Zahlen, diese rechne ich einfach plus 1 um die Zeit dann hochscrollen zu lassen
+    //Die Ifs fragen außerdem noch ab ob die Zahlen valid sind
+    //Da es keine 10 oder 6 geben kann bei einem einstelligen Sekunden block z.B. 67 oder 107
+    elementLeft2!.innerHTML = [10, 6].includes((+timeElement[0] + 1)) ? '0' : (+timeElement[0] + 1).toString()
+    elementRight2!.innerHTML = (+timeElement[1] + 1) == 10 ? '0' : (+timeElement[1] + 1).toString();
+  } else {
+    elementLeft!.innerHTML = '0';
+    elementRight!.innerHTML = timeElement[0];
+    elementLeft2!.innerHTML = '1';
+    elementRight2!.innerHTML = (+timeElement[0] + 1) == 10 ? '0' : (+timeElement[0] + 1).toString();
+  }
+
+  if (doMove) {
+    if (elementRightClassList.contains('move') || !parentHasMove) {
+      elementRightClassList.remove('move');
+
+      elementRight2ClassList.remove('move');
+    } else if (parentHasMove) {
+      elementRightClassList.add('move');
+
+      elementRight2ClassList.add('move');
+    }
+
+    //Hier werden die Moveelemente von den Zehnerszahlen entfernt
+    if (elementLeftClassList.contains('move')) {
+      elementLeftClassList.remove('move');
+
+      elementLeft2ClassList.remove('move');
+      //Mit diesem If wird abgefragt ob die neunte Zahl im 10er Block erreicht ist um die Animation für die 10er Zahlen zu starten.
+    } else if (+timeElement % 10 == 9 && elementRightClassList.contains('move')) {
+      elementLeftClassList.add('move');
+
+      elementLeft2ClassList.add('move');
+    }
+  }
+}
+
+function refreshWrapper(currDate: Date) {
+  refreshSeconds('second', currDate.getSeconds().toString(), true, true);
+
+  if (currDate.getSeconds() == 59 || currDate.getSeconds() == 0) {
+    refreshSeconds('minute', currDate.getMinutes().toString(), true, document.getElementById("secondLeft")!.classList.contains('move'));
+
+    if (currDate.getMinutes() == 59 || currDate.getMinutes() == 0) {
+      refreshSeconds('hour', currDate.getHours().toString(), true, document.getElementById("secondLeft")!.classList.contains('move'));
+    }
+  }
+}
